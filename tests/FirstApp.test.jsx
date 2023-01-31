820ddfca678a15e76089388b4201d7370550ca33
Fernando Nicolás <fersanti2896@gmail.com>
2023-01-31 14:38:53 -0600

    Creando fotografía del componente FirstApp y pruebas al componente.

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -3,6 +3,20 @@ import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas en componente <FirstApp />', () => {
     test('Debe hacer match con el snapshot.', () => {
-        render(<FirstApp />)
+        const { container } = render(<FirstApp />);
+
+        expect( container ).toMatchSnapshot();
+    });
+
+    test('Debe mostrar el título en un h1.', () => {
+        const title = '¡Bienvenido al curso React!';
+        const { container, getByText } = render(<FirstApp title={title}/>);
+
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        console.log(h1.innerHTML);
+
+        expect(h1.innerHTML).toContain(title);
     });
 });
